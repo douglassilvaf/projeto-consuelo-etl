4dcec450710f38b72ca890fc13f32e3217b52970
douglassilvaf <douglas97.15silva@gmail.com>
2024-09-16 13:54:45 -0300

    merge agrupamento

--- agrupamento_grafico.py
+++ agrupamento_grafico.py
@@ -1,18 +1,24 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-#Carregar o arquivo CSV
+# Carregar o arquivo CSV
 df = pd.read_csv("geracao_usina_agrupado.csv")
 df = df.drop(columns=['ESTADO', 'REGIAO'])
 
+df['MES'] = df['MES'].astype(str)
+
+# Agrupar os dados por 'MES' e 'TIPO_DE_USINA' e somar os valores
 resultado = df.groupby(['MES', 'TIPO_DE_USINA']).sum().reset_index()
 
+# Definir o custo por MWh
 custo_mwh = 67.70
 
 resultado['VALOR_PARA_GERAR_R$'] = (resultado['VALOR_PARA_GERAR'] * custo_mwh)/1000000.00
+# Remover linhas onde a coluna 'MES' tem o valor '9'
+resultado = resultado[resultado['MES'] != '9']
 
-resultado = resultado.drop(columns=['VALOR_PARA_GERAR'])
-
+# Exibir o DataFrame resultado
 print(resultado)
 
+# Salvar o DataFrame resultado em um novo arquivo CSV
 resultado.to_csv("geracao_usina_grafico.csv", index=False)
